849c85b5c6d5f8d693b93805dc30cd1abf40cacf
Leonardo Jimenez <jimenezleonardop@gmail.com>
2018-04-02 14:23:32 -0400

    Fix chain param

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -178,7 +178,12 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (   0, uint256S("0x000007be8267416ac083a55c7ae1cf7e91c17455913b0294c0a4b0a8cca2ca11"))
+            (   0, uint256S("0x000007be8267416ac083a55c7ae1cf7e91c17455913b0294c0a4b0a8cca2ca11")),
+
+            1522249533, // * UNIX timestamp of last checkpoint block
+            1,       // * total number of transactions between genesis and last checkpoint
+                        //   (the tx=... number in the SetBestChain debug.log lines)
+            0         // * estimated number of transactions per day after checkpoint
         };
     }
 };
